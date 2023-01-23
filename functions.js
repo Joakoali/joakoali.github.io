8b006a90b86072bab50fe6e30a56383678b1191f
Joakoali <109369263+Joakoali@users.noreply.github.com>
2023-01-23 13:18:45 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,60 @@
+function mostrar(){
+    var oct = document.getElementById("obj");
+    oct.style.display ='block';
+}
+function ocultar(){
+    var oct = document.getElementById("obj");
+    oct.style.display='none';
+}
+var mos = document.getElementById("objetivo");
+mos.onmouseover = mostrar;
+mos.onmouseout = ocultar;
+
+function mostrar1(){
+    var oct = document.getElementById("formA");
+    oct.style.display ='block';
+}
+function ocultar1(){
+    var oct = document.getElementById("formA");
+    oct.style.display='none';
+}
+var mos = document.getElementById("formacionA");
+mos.onmouseover = mostrar1;
+mos.onmouseout = ocultar1;
+
+function mostrar2(){
+    var oct = document.getElementById("exp");
+    oct.style.display ='block';
+}
+function ocultar2(){
+    var oct = document.getElementById("exp");
+    oct.style.display='none';
+}
+var mos = document.getElementById("experiencia");
+mos.onmouseover = mostrar2;
+mos.onmouseout = ocultar2;
+
+function mostrar3(){
+    var oct = document.getElementById("formC");
+    oct.style.display ='block';
+}
+function ocultar3(){
+    var oct = document.getElementById("formC");
+    oct.style.display='none';
+}
+var mos = document.getElementById("formacionC");
+mos.onmouseover = mostrar3;
+mos.onmouseout = ocultar3;
+
+function mostrar4(){
+    var oct = document.getElementById("cont");
+    oct.style.display ='block';
+}
+function ocultar4(){
+    var oct = document.getElementById("cont");
+    oct.style.display='none';
+}
+var mos = document.getElementById("contacto");
+mos.onmouseover = mostrar4;
+mos.onmouseout = ocultar4;
+
